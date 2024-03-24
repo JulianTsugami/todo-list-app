18ceb17fea43eb5692295ce12c6cb1696ecc2057
Author: JulianTsugami <julian.tsugami@icloud.com>
2024-03-24 20:19:21 +0900

    Create all endpoints

--- todo-list-app-backend/src/main/java/com/juliantsugami/todolistappbackend/ItemController.java
+++ todo-list-app-backend/src/main/java/com/juliantsugami/todolistappbackend/ItemController.java
@@ -2,17 +2,66 @@ package com.juliantsugami.todolistappbackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Controller
+@RequestMapping(path = "/v1/items")
 public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
 
-    @GetMapping(path="/")
-    public @ResponseBody Iterable<Item> getTodoList() {
-        // This returns a JSON or XML with the users
+    @GetMapping(path = { "", "/" })
+    public @ResponseBody Iterable<Item> getAllItems() {
         return itemRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Optional<Item> getItem(@PathVariable("id") Integer id) throws Exception {
+        if (!itemRepository.existsById(id)) {
+            throw new Exception("Item not found with id " + id);
+        }
+        return itemRepository.findById(id);
+    }
+
+    @PostMapping(path = { "", "/" })
+    public @ResponseBody Item createItem(@RequestParam String description) {
+        Item newItem = new Item();
+        newItem.setDescription(description);
+        newItem.setIsDone(false);
+        Date now = new Date();
+        newItem.setCreatedAt(now);
+        newItem.setUpdatedAt(now);
+        itemRepository.save(newItem);
+
+        return newItem;
+    }
+
+    @PutMapping(path = "/{id}")
+    public @ResponseBody Optional<Item> updateItem(@PathVariable("id") Integer id,
+            @RequestParam String description, @RequestParam Boolean isDone) throws Exception {
+        Item existingItem = itemRepository.findById(id)
+                .orElseThrow(() -> new Exception("Item not found with id " + id));
+
+        if (!existingItem.getDescription().equals(description) || !existingItem.getIsDone().equals(isDone)){
+            existingItem.setDescription(description);
+            existingItem.setIsDone(isDone);
+            existingItem.setUpdatedAt(new Date());
+
+            return Optional.of(itemRepository.save(existingItem));
+        }
+
+        return Optional.empty();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public @ResponseBody String deleteItem(@PathVariable("id") Integer id) throws Exception {
+        if (!itemRepository.existsById(id)) {
+            throw new Exception("Item not found with id " + id);
+        }
+        itemRepository.deleteById(id);
+        return "Deleted";
     }
 }
--- todo-list-app-backend/src/main/java/com/juliantsugami/todolistappbackend/ItemRepository.java
+++ todo-list-app-backend/src/main/java/com/juliantsugami/todolistappbackend/ItemRepository.java
@@ -2,8 +2,6 @@ package com.juliantsugami.todolistappbackend;
 
 import org.springframework.data.repository.CrudRepository;
 
-import  com.juliantsugami.todolistappbackend.Item;
-
 public interface ItemRepository extends CrudRepository<Item, Integer> {
 
 }
